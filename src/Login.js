fff1af8912ea884623cabe5f2590d0b64d42b859
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-08-10 23:17:43 +0530

    added manage profile permission

---
+++ src/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import fblogo from "./Facebook-logo.png";
+export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+		invokeLoginModal = () => {
+			if(typeof(FB) != undefined	){
+				/*global FB*/
+				FB.login(function(response){
+					console.log(response);
+				}, {
+						scope: 'public_profile,email,manage_pages'
+				});
+			}
+		}
+
+    render(){
+        return(
+            <div style={{
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                height: '100vh'
+            }}>
+                <div>
+                    <h1>Please login to continue</h1>
+                    <button onClick={this.invokeLoginModal}>
+                        <img src={fblogo} style={{width:"150px",height:"90px"}}/>
+                    </button>
+                </div>
+            </div>
+            
+        )
+    }
+}
