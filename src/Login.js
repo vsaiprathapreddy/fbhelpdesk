b56aa0d444b8111db95713b4950d14300d218a10
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-08-11 20:38:37 +0530

    eroor rectified

--- src/Login.js
+++ src/Login.js
@@ -19,9 +19,7 @@ export default class Login extends React.Component {
                         if (token) {
                             sessionStorage.setItem('token', token);
                             window.location = "/dashboard"
-                        // } else {
-                        //     alert('Login Failed')
-                        // }
+                        }
                     }, {
                         scope: 'public_profile,email'
                     });
