64ba3f2edabce0f521609def1bbd536fffad2d0a
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-08-11 20:03:24 +0530

    typo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -212,7 +212,7 @@ export default function HomePage(props) {
           {['Inbox', 'Drafts', 'Starred', 'Send email'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={text == 'Drafts' ? 'Conversations' : text} />
             </ListItem>
           ))}
         </List>
